9b6e63d1bc1a211fa3524a25b529f01e03b3f1c2
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-04-22 03:44:11 -0400

    added database

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// db connection
+require("./config/db")();
+
 app.use(express.json());
 
 // login
-app.use("/", require("./routes/login"));
+app.use("/login", require("./routes/login"));
+// register
+app.use("/register", require("./routes/register"));
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
