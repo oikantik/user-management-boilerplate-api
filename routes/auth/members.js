d603005d57da5c0fe547e812baab3261a1f7b2e1
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-04-26 01:53:16 -0400

    updated members.js

--- routes/auth/members.js
+++ routes/auth/members.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const User = require("../../models/User");
 
 router.get("/", (req, res, next) => {
-  passport.authenticate("jwt", (err, user) => {
+  passport.authenticate("jwt", async (err, user) => {
     if (err)
       return res.status(400).json({
         success: false,
@@ -14,9 +15,13 @@ router.get("/", (req, res, next) => {
         success: false,
         message: "User Not Found",
       });
+    const countMembers = await User.estimatedDocumentCount();
+    const allMembers = await User.find({}, "name email dob gender");
     res.status(200).json({
       success: true,
       message: "Login Successful",
+      members: allMembers,
+      count: countMembers,
     });
   })(req, res, next);
 });
