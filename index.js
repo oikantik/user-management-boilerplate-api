1f3d9a56f1937c12a87693b088eded12a8c472eb
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-04-26 00:29:49 -0400

    added cross domain cookie

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ require("./config/db")();
 // setup cors
 const corsSettings = {
   origin: "http://localhost:3000",
+  credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
